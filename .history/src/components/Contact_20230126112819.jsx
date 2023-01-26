5957af3c6f86bec5adc56655df9c2fe590f04f16
Tomiogunsan <tomiafolayan@gmail.com>
2023-01-26 12:24:07 +0100

    added map to the listing

---
+++ .history/src/components/Contact_20230126112819.jsx
@@ -0,0 +1,56 @@
+import { doc, getDoc } from 'firebase/firestore'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
+
+export default function Contact({userRef, listing}) {
+    const [landlord, setLandlord] = useState(null)
+    const [message, setMessage] = useState('')
+    useEffect(()=> {
+        async function getLandlord(){
+            const docRef = doc(db, 'users', userRef )
+            const docSnap = await getDoc(docRef);
+            if(docSnap.exists()){
+                setLandlord(docSnap.data())
+            }else {
+                toast.error('Could not get landlord data')
+            }
+        }
+        getLandlord();
+    }, [userRef])
+
+    function onChange(e){
+        setMessage(e.target.value)
+    }
+  return (
+    <>{landlord !== null && (
+        <div className='flex flex-col w-full '>
+            <p >Contact {landlord.name} for the {listing.name.toLowerCase()}</p>
+            <div className='mt-1 mb-3'>
+                <textarea className='w-full px-4 py-2 
+                text-xl text-gray-700 border border-gray-300 rounded transition duration-150 
+                ease-in-out focus:text-gray-700 focus:bg-white
+                focus:border-slate-600'
+                name="message"
+                 id="message"
+                  rows="2"
+                  value={message}
+                  onChange={onChange}>
+
+                  </textarea>
+            </div>
+            <a href={`mailto:${landlord.email}?Subject=${listing.name}&body=${message}`}>
+               <button type='button'
+                className=' mb-6 bg-blue-600 text-white px-7 py-3 rounded text-sm
+                uppercase shadow-md hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg 
+                active:shadow-lg active:bg-blue-700 transition duration-150 ease-in-out
+                w-full text-center ' >
+                Send Message
+                </button> 
+            </a>
+        </div>
+    )}</>
+  )
+}
